a7daef37768b6b890d87f46af90090ee6700a262
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-24 17:08:14 -0700

    Can now track the baseball. Now I just need to look at some stuff to figure out what it is

--- HW1/baseballTracker.cpp
+++ HW1/baseballTracker.cpp
@@ -27,8 +27,27 @@ Mat cleanUpNoise(Mat noisy_img)
   Mat img;
   Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
   erode(noisy_img, img, element);
-  // element = getStructuringElement(MORPH_RECT, Size(5, 5), Point(0, 0));
   dilate(img, img, element);
+
+  return img;
+}
+
+Mat findCentroids(Mat diff, Mat img)
+{
+  Mat canny_out;
+  Canny(diff, canny_out, 100, 200, 3);
+  std::vector<std::vector<Point>> contours;
+  findContours(canny_out, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+  std::vector<Moments> mu(contours.size());
+  std::vector<Point2f> mc(contours.size());
+  for(int i(0); i< contours.size(); i++)
+  {
+    mu[i] = moments(contours[i]);
+    mc[i] = Point2f(static_cast<float>(mu[i].m10/(mu[i].m00+1e-5)),
+                    static_cast<float>(mu[i].m01/(mu[i].m00+1e-5)));
+    circle(img, mc[i], 30, Scalar(0, 0, 255), 3, 8);
+  }
 
   return img;
 }
@@ -60,9 +79,10 @@ int main()
 
       img2 = imread(filepath + filename + number + filetype);
       diff = absoluteDifference(img1, img2);
-      diff = computeThreshold(diff, 15, 255, 0);
-
+      diff = computeThreshold(diff, 17, 255, 0);
       diff = cleanUpNoise(diff);
+      diff = findCentroids(diff, img2);
+
       imshow("Baseball", diff);
 
       // img1 = img2;
