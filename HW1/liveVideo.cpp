838d2026280c138569eadb0f27e5834be4a504cc
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-05 16:07:14 -0700

    Changed the live video file to record pictures so that I can take pictures for task 5 and 6

--- HW1/liveVideo.cpp
+++ HW1/liveVideo.cpp
@@ -7,21 +7,28 @@ using namespace cv;
 int main()
 {
         VideoCapture video(0);
+        std::string filename, filetype;
+        filename = "../../HW2/my_calibration_images/AR";
+        filetype = ".jpg";
+        int counter = 1;
 
         if(!video.isOpened())
                 std::cout << "Cannot open camera\n";
 
+        Mat frame, gray_frame;
         while(true)
         {
-                Mat frame;
                 video >> frame;
 //                namedWindow("Forsgren", CV_WINDOW_AUTOSIZE);
                 imshow("Forsgren", frame);
 
                 if(waitKey(30) >= 0)
-                        break;
+                {
+                        cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
+                        imwrite(filename + std::to_string(counter) + filetype, gray_frame);
+                        counter++;
+                }
         }
 
         return 0;
 }
-
