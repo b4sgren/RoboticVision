cd8012d75440ad2e67a49f096846332edbbac3b3
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-19 20:46:06 -0700

    Finished task 1. Individual calibration is complete and the intrinsics are written to a file

--- HW3/camera_calibration.cpp
+++ HW3/camera_calibration.cpp
@@ -9,19 +9,28 @@ void intrinsicCalibrate(std::string input_file, std::string output_file);
 
 int main()
 {
-  // std::string path("../calibration_images/AR");
-  std::string path("../my_calibration_images/AR");
+  std::string left_imgs_file("../calibration_imgs/leftCalibrate/aL");
+  std::string left_output_file("leftIntrinsics.txt");
+  std::string right_imgs_file("../calibration_imgs/rightCalibrate/aR");
+  std::string right_output_file("rightIntrinsics.txt");
 
-  std::string filetype(".jpg");
-  // int num_pics(41); //for Dr. Lee's camera
-  int num_pics(25); //my camera
+  intrinsicCalibrate(left_imgs_file, left_output_file); //Done so don't need to call again
+  intrinsicCalibrate(right_imgs_file, right_output_file); // Done so don't need to call again
+
+  return 0;
+}
+
+void intrinsicCalibrate(std::string input_file, std::string output_file)
+{
+  std::string filetype(".bmp");
+  int num_pics(100); //my camera
 
   cv::Mat img, g_img;
   std::vector<std::vector<cv::Point3f>> object_points; //vector of points of corners in 3D space. All of these are the same
   std::vector<std::vector<cv::Point2f>> image_points; //vector of vectors with corner locations
 
-  // cv::Size pattern_size(10, 7); //For Dr. Lees
   cv::Size pattern_size(9, 7);
+  double size = 3.88636; // See baseball pdf for last 3 digits
 
   int flags(cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE);
   cv::TermCriteria criteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.01);
@@ -32,53 +41,39 @@ int main()
   {
     for(int j(0); j < pattern_size.width; j++)
     {
-      //Assume the width of each square is 2 inches
-      points_3d.push_back(cv::Point3f(2.0*j, 2.0*i, 0.0));
+      points_3d.push_back(cv::Point3f(size*j, size*i, 0.0));
     }
   }
 
-  for(int i(1); i<num_pics; i++)
+  std::string filename;
+  for(int i(0); i<num_pics; i++)
   {
-    img = cv::imread(path + std::to_string(i) + filetype);
+    if(i < 10)
+      filename = input_file + std::to_string(0) + std::to_string(i) + filetype;
+    else
+      filename = input_file + std::to_string(i) + filetype;
+
+    img = cv::imread(filename);
     cv::cvtColor(img, g_img, cv::COLOR_RGB2GRAY);
 
     std::vector<cv::Point2f> corners;
     corners.clear();
     bool pattern_found = findChessboardCorners(g_img, pattern_size, corners, flags);
-    if(!pattern_found)
+    if(pattern_found)
     {
-      std::cout << "Failed on picutre " << i << std::endl;
-      return -1;
+      cornerSubPix(g_img, corners, cv::Size(3, 3), cv::Size(-1, -1), criteria);
+      image_points.push_back(corners);
+      object_points.push_back(points_3d);
     }
-    cornerSubPix(g_img, corners, cv::Size(3, 3), cv::Size(-1, -1), criteria);
-
-    image_points.push_back(corners);
-    object_points.push_back(points_3d);
   }
-
   cv::Mat camera_matrix, dst_coeffs;
   std::vector<cv::Mat> r_vecs, t_vecs;
   cv::calibrateCamera(object_points, image_points, g_img.size(), camera_matrix, dst_coeffs, r_vecs, t_vecs);
 
-  //Get focal length camera_mat = [fx s cx; 0 fy cy; 0 0 1]
-  // double pix_size = 4.7e-3; //Use for Dr. Lees camera
-  double pix_size = 1; //my camera
-
-  double fx = camera_matrix.at<double>(0, 0) * pix_size;
-  double fy = camera_matrix.at<double>(1, 1) * pix_size;
-
   std::cout << "Intrinsic Parameters:\n" << camera_matrix << std::endl << std::endl;
   std::cout << "Distortion Parameters:\n" << dst_coeffs << std::endl << std::endl;
 
-  cv::FileStorage fout("camera_params.txt", cv::FileStorage::WRITE);
-  // cv::FileStorage fout("my_camera_params.txt", cv::FileStorage::WRITE);
+  cv::FileStorage fout(output_file, cv::FileStorage::WRITE);
   fout << "Camera_Matrix" << camera_matrix << "Distortion_Params" << dst_coeffs;
   fout.release();
-
-  return 0;
-}
-
-void intrinsicCalibrate(std::string input_file, std::string output_file)
-{
-  
 }
