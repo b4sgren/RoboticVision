7681bf72c1de664210c864c516c7c5245a9d2200
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-23 11:39:01 -0700

    The remap makes small changes that become apparent in the absolute difference image. Now I just need to draw lines on the rectified images

--- HW3/rectify.cpp
+++ HW3/rectify.cpp
@@ -10,6 +10,15 @@ void readFile(std::string filename, cv::Mat &camera_mat, cv::Mat &dist_coeff)
   fin.release();
 }
 
+cv::Mat absoluteDifference(cv::Mat gray_frame, cv::Mat prev_frame)
+{
+  cv::Mat image;
+  cv::absdiff(prev_frame, gray_frame, image);
+
+  return image;
+}
+
+
 int main()
 {
   std::string left_img("../calibration_imgs/combinedCalibrate/aL76.bmp");
@@ -40,6 +49,22 @@ int main()
   cv::Mat map1L, map2L, map1R, map2R; // Not sure I need 4. His slides make it sound like I just need 2
   cv::initUndistortRectifyMap(camera_matL, dist_coeffL, R1, P1, g_imgL.size(), CV_32FC1, map1L, map2L);
   cv::initUndistortRectifyMap(camera_matR, dist_coeffR, R2, P2, g_imgR.size(), CV_32FC1, map1R, map2R);
+
+  cv::Mat outputL, outputR;
+  cv::remap(g_imgL, outputL, map1L, map2L, cv::INTER_LINEAR);
+  cv::remap(g_imgR, outputR, map1R, map2R, cv::INTER_LINEAR);
+
+  cv::Mat absL, absR;
+  absL = absoluteDifference(g_imgL, outputL);
+  absR = absoluteDifference(g_imgR, outputR);
+
+  cv::imshow("Left Original", g_imgL);
+  cv::imshow("Left Remap", outputL);
+  cv::imshow("Left Abs", absL);
+  cv::imshow("Right Original", g_imgR);
+  cv::imshow("Right Remap", outputR);
+  cv::imshow("Right Abs", absR);
+  cv::waitKey();
 
   return 0;
 }
