19d21743e30e31e0d196c7407e86e8a868330ece
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-05 19:02:51 -0700

    Read in the desired images

--- HW4/3dmes.cpp
+++ HW4/3dmes.cpp
@@ -1,7 +1,20 @@
 #include <opencv2/opencv.hpp>
 
+#include <string>
+#include <vector>
+
 int main()
 {
+  std::string filenameL{"../../HW3/calibration_imgs/combinedCalibrate/aL29.bmp"};
+  std::string filenameR{"../../HW3/calibration_imgs/combinedCalibrate/aR29.bmp"};
+
+  cv::Mat imgL, imgR;
+  imgL = cv::imread(filenameL);
+  imgR = cv::imread(filenameR);
+
+  cv::imshow("Left", imgL);
+  cv::imshow("Right", imgR);
+  cv::waitKey(0);
 
   return 0;
 }
