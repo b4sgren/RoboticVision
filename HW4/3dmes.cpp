b27d839c51bbcae467b985abc1dab4a7fc2e852d
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-05 19:13:32 -0700

    Done with the findCorners and cornerSubPix function

--- HW4/3dmes.cpp
+++ HW4/3dmes.cpp
@@ -8,9 +8,22 @@ int main()
   std::string filenameL{"../../HW3/calibration_imgs/combinedCalibrate/aL29.bmp"};
   std::string filenameR{"../../HW3/calibration_imgs/combinedCalibrate/aR29.bmp"};
 
-  cv::Mat imgL, imgR;
+  cv::Mat imgL, imgR, g_imgL, g_imgR;
   imgL = cv::imread(filenameL);
   imgR = cv::imread(filenameR);
+
+  cv::cvtColor(imgL, g_imgL, cv::COLOR_BGR2GRAY);
+  cv::cvtColor(imgR, g_imgR, cv::COLOR_BGR2GRAY);
+
+  cv::Size pattern_size(10, 7);
+  std::vector<cv::Point2f> cornersL, cornersR;
+  int flags(cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE);
+  bool foundL = cv::findChessboardCorners(g_imgL, pattern_size, cornersL, flags);
+  bool foundR = cv::findChessboardCorners(g_imgR, pattern_size, cornersR, flags);
+
+  cv::TermCriteria criteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.001);
+  cv::cornerSubPix(g_imgL, cornersL, cv::Size(5,5), cv::Size(-1,-1), criteria);
+  cv::cornerSubPix(g_imgR, cornersR, cv::Size(5,5), cv::Size(-1,-1), criteria);
 
   cv::imshow("Left", imgL);
   cv::imshow("Right", imgR);
