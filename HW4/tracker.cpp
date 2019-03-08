2644ee71b8751cd177c33da73cd13aa020226877
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-08 12:28:04 -0700

    Binarized and filtered the img. Now I just need to detect the baseball and do the rectify stuff on it

--- HW4/tracker.cpp
+++ HW4/tracker.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <string>
 
+cv::Mat absoluteDifference(cv::Mat gray_frame, cv::Mat prev_frame);
+cv::Mat computeThreshold(cv::Mat gray_frame, int thresh, int high_val, int type);
+cv::Mat cleanUpNoise(cv::Mat noisy_img);
+
 int main()
 {
   std::string filenameL("../baseball/ballL"), filenameR("../baseball/ballR");
@@ -15,9 +19,59 @@ int main()
   cv::cvtColor(imgL, backgroundL, cv::COLOR_BGR2GRAY);
   cv::cvtColor(imgR, backgroundR, cv::COLOR_BGR2GRAY);
 
-  cv::imshow("Left", imgL);
-  cv::imshow("Right", imgR);
-  cv::waitKey(0);
+  cv::Mat g_imgL, g_imgR;
+  for(int i(1); i < num_files; i++)
+  {
+    std::string file_num;
+    if(i < 10)
+      file_num = "0" + std::to_string(i);
+    else
+      file_num = std::to_string(i);
+
+    imgL = cv::imread(filenameL + file_num + file_ext);
+    imgR = cv::imread(filenameR + file_num + file_ext);
+    cv::cvtColor(imgL, g_imgL, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(imgR, g_imgR, cv::COLOR_BGR2GRAY);
+
+    g_imgL = absoluteDifference(g_imgL, backgroundL);
+    g_imgR = absoluteDifference(g_imgR, backgroundR);
+
+    g_imgL = computeThreshold(g_imgL, 17, 255, 0);
+    g_imgR = computeThreshold(g_imgR, 17, 255, 0);
+
+    g_imgL = cleanUpNoise(g_imgL);
+    g_imgR = cleanUpNoise(g_imgR);
+
+    cv::imshow("Left", g_imgL);
+    cv::imshow("Right", g_imgR);
+    cv::waitKey(0);
+  }
 
   return 0;
+}
+
+cv::Mat absoluteDifference(cv::Mat gray_frame, cv::Mat prev_frame)
+{
+  cv::Mat image;
+  cv::absdiff(prev_frame, gray_frame, image);
+
+  return image;
+}
+
+cv::Mat computeThreshold(cv::Mat gray_frame, int thresh, int high_val, int type)
+{
+  cv::Mat image;
+  cv::threshold(gray_frame, image, thresh, high_val, type);
+
+  return image;
+}
+
+cv::Mat cleanUpNoise(cv::Mat noisy_img)
+{
+  cv::Mat img;
+  cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+  cv::erode(noisy_img, img, element);
+  cv::dilate(img, img, element);
+
+  return img;
 }
