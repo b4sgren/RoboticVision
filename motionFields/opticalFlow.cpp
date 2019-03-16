383d15ab92af4cf5f04ef20753340803919d6e5b
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-16 14:52:20 -0600

    The optical flow calculation runs. Now to visualize to see if it works

--- motionFields/opticalFlow.cpp
+++ motionFields/opticalFlow.cpp
@@ -1,4 +1,8 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <vector>
+#include <iostream>
 
 int main()
 {
@@ -6,17 +10,30 @@ int main()
 
   cv::Mat prev_img, img;
   cap >> prev_img;
+  cv::cvtColor(prev_img, prev_img, cv::COLOR_BGR2GRAY);
 
+  int max_corners(500);
+  double quality(0.01), min_dist(50.0);
   while(true)
   {
     cap >> img;
     if(img.empty())
       break;
+    cv::Mat g_img;
+    cv::cvtColor(img, g_img, cv::COLOR_BGR2GRAY);
 
-    cv::imshow("MotionField", img);
-    cv::waitKey(30);
+    std::vector<cv::Point2f> prev_corners;
+    cv::goodFeaturesToTrack(prev_img, prev_corners, max_corners, quality, min_dist);
 
-    img.copyTo(prev_img);
+    std::vector<uchar> status;
+    std::vector<cv::Point2f> corners;
+    std::vector<float> err;
+    cv::calcOpticalFlowPyrLK(prev_img, g_img, prev_corners, corners, status, err);
+
+    // cv::imshow("MotionField", img);
+    // cv::waitKey(30);
+
+    g_img.copyTo(prev_img);
   }
 
   return 0;
