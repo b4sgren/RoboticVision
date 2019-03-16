55d5220c974904f2bf6ba33ba71ffca7117dd3ee
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-16 14:18:01 -0600

    Added the video that the motion fields will be done on.

--- motionFields/opticalFlow.cpp
+++ motionFields/opticalFlow.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
+  cv::VideoCapture cap("../MotionFieldVideo.mp4");
+
+  cv::Mat img;
+
+  while(true)
+  {
+    cap >> img;
+    if(img.empty())
+      break;
+
+    cv::imshow("MotionField", img);
+    cv::waitKey(30);
+  }
 
   return 0;
 }
