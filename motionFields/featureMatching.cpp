9a1f4669127b02afc488f587545803da32c6f52d
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-20 18:29:33 -0600

    Not sure how to make it go faster. Will look at it more tomorrow

--- motionFields/featureMatching.cpp
+++ motionFields/featureMatching.cpp
@@ -19,7 +19,7 @@ void skipFrames(int n_frames)
     prev_imgs.push(g_img);
   }
 
-  int max_corners(500), side(15);
+  int max_corners(500), side(100);
   double quality(0.01), min_dist(10.0);
   cv::Size template_size{side, side};
   int match_method = cv::TM_SQDIFF;
@@ -59,11 +59,12 @@ void skipFrames(int n_frames)
     }
 
     //Find matches
+    //https://docs.opencv.org/4.0.1/de/lda9/tutorial_template_matching.html
     for(cv::Mat templ : templates)
     {
       cv::Mat result;
       result.create(g_img.rows, g_img.cols, CV_32FC1);
-      cv::matchTemplate(g_img, templ, result, match_method); //This takes forever
+      cv::matchTemplate(g_img, templ, result, match_method); //This takes forever. How to go faster??
     }
 
     cv::imshow("MotionField", img);
