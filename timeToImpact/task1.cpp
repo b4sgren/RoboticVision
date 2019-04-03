6aa25582b27ce755a2fc2e87489241b4e3b776b6
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-02 18:49:44 -0600

    Read in each file image

--- timeToImpact/task1.cpp
+++ timeToImpact/task1.cpp
@@ -14,6 +14,17 @@ int main()
   fin["Camera_Matrix"] >> M;
   fin["Distortion_Params"] >> dst;
 
-  
+  cv::Mat img, g_img, img_prev, g_prev;
+  img_prev = cv::imread(path + "1" + filetype);
+  cv::cvtColor(img_prev, g_prev, cv::COLOR_BGR2GRAY);
+  for(int i(2); i < 19; i++)
+  {
+    img = cv::imread(path + std::to_string(i) + filetype);
+    cv::cvtColor(img, g_img, cv::COLOR_BGR2GRAY);
+
+    cv::imshow("Image", img);
+    cv::waitKey(0);
+  }
+
   return 0;
 }
