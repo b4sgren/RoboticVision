d05343ca27f2eeb5adf7c99ce8ee4ab851f7b591
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-19 11:14:28 -0600

    Small adjustments to the yaml file

--- visualOdom/task1.cpp
+++ visualOdom/task1.cpp
@@ -14,10 +14,22 @@ int main()
   for(size_t i{0}; i < result.gl_pathc; i++)
     filenames.push_back(std::string(result.gl_pathv[i])); // puts them in order
 
+  cv::FileStorage fin{"../PracticeImgs/CameraParameters.yaml", cv::FileStorage::READ};
+  cv::Mat M;
+  fin["Camera_Matrix"] >> M;
+  fin.release();
+
   cv::Mat img, g_img, key_frame, g_key_frame;
   key_frame = cv::imread(filenames[0]);
   cv::cvtColor(key_frame, g_key_frame, cv::COLOR_BGR2GRAY);
 
+  for(int i{1}; i < filenames.size(); i++)
+  {
+    img = cv::imread(filenames[i]);
+    cv::cvtColor(img, g_img, cv::COLOR_BGR2GRAY);
+    cv::imshow("Frame", img);
+    cv::waitKey(0);
+  }
 
   return 0;
 }
